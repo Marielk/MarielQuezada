ac1dda99f154490fbb0a89b4cb851861bfe58c5b
Mariel <marielk.mya@gmail.com>
2018-10-13 14:02:09 -0300

    menu desplegable responsive

--- src/app/vistas/top-menu/responsive-menu/responsive-menu.component.ts
+++ src/app/vistas/top-menu/responsive-menu/responsive-menu.component.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./responsive-menu.component.css']
 })
 export class ResponsiveMenuComponent implements OnInit {
-
-  constructor() { }
+  form: any = document.getElementsByName('formNav');
+  // formEntrie: any = ;
+  // formEntrieDeep: any = Object.entries(this.formEntrie);
+  input: any;
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+  // turnOn() {
+  //   const input = this.form[0][0];
+  //   const inputArray = [this.form[0][1], this.form[0][2], this.form[0][3], this.form[0][4], this.form[0][5], this.form[0][6]];
+  //   const aElement = document.getElementsByTagName('a');
+  //   const aElemArray = [aElement[1], aElement[2], aElement[3], aElement[4], aElement[5], aElement[6] ];
+  //   // console.log(inputArray);
+  //   // console.log(aElemArray);
+
+  //   if (input.checked === true) {
+  //     // console.log('true');
+  //     inputArray.forEach(checkbox => {
+  //       checkbox.disabled = false;
+  //       checkbox.style.display = 'inline-block';
+  //     });
+  //     aElemArray.forEach( a => {
+  //       a.style.display = 'inline-block';
+  //     });
+  //   } else {
+  //     // console.log('false');
+  //     inputArray.forEach(checkbox => {
+  //       checkbox.disabled = true;
+  //     });
+  //     // console.log(inputArray[0].disabled);
+  //     // console.log(input.checked);
+  //   }
+  // }
+
+  validate() {
+    const input = this.form[0][0];
+    if (input.checked === true) {
+      input.checked = false;
+    }
+    // console.log(this.input.checked);
+  }
+  // on() {
+  //   this.input.value = 'on';
+  // }
+  // return() {
+  //   this.input.nodeValue = 'off';
+  // }
 
 }
